16178ebba0de1369c397f21e8f9240f4f9b1a57e
Hagen Möbius <hagen.moebius@googlemail.com>
2006-08-11 17:21:09 +0000

    Added a function to set the position.

--- position.h
+++ position.h
@@ -6,9 +6,15 @@
 class Position
 {
 public:
+	void SetPosition(const math3d::vector2f & Position);
 	const math3d::vector2f & GetPosition(void) const;
 	math3d::vector2f m_Position;
 };
+
+inline void Position::SetPosition(const math3d::vector2f & Position)
+{
+	m_Position = Position;
+}
 
 inline const math3d::vector2f & Position::GetPosition(void) const
 {
