fd12be0331f82de351eae306d537f8c3862dcff5
Hagen Möbius <hagen.moebius@googlemail.com>
2006-08-12 09:00:00 +0000

    Added two functions to return internal state of cargo: the commodity and the radial size of the object.

--- cargo.h
+++ cargo.h
@@ -15,6 +15,8 @@ public:
 	Cargo(Model * Model, const Commodity * Commodity, const math3d::vector2f & Velocity);
 	void Draw(void) const;
 	void Move(float Seconds);
+	const Commodity * GetCommodity(void) const;
+	float GetRadialSize(void) const;
 private:
 	Model * m_Model;
 	const Commodity * m_Commodity;
@@ -23,5 +25,15 @@ private:
 	math3d::vector3f m_RotationAxis;
 	float m_AngularVelocity;
 };
+
+inline const Commodity * Cargo::GetCommodity(void) const
+{
+	return m_Commodity;
+}
+
+inline float Cargo::GetRadialSize(void) const
+{
+	return 0.867f;  // sqrt(sqr(0.5) + sqr(0.5) + sqr(0.5))
+}
 
 #endif
