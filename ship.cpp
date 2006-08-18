f2f358fce5fb6e6466d9912135062190337e92da
hagen.moebius <hagen.moebius@localhost>
2006-08-18 23:37:17 +0000

    Made the ShipClass object-oriented by adding getters and setters and making the mamber variables private. Added reading, setting and getting the fuel-hold-size but not evaluating it yet.

--- ship.cpp
+++ ship.cpp
@@ -15,7 +15,7 @@ void Ship::Draw(void) const
 	glTranslatef(m_Position.m_V.m_A[0], m_Position.m_V.m_A[1], 0.0f);
 	glRotatef(m_AngularPosition * 180.0f / M_PI, 0.0f, 0.0f, 1.0f);
 	glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-	m_ShipClass->m_Model->Draw();
+	m_ShipClass->GetModel()->Draw();
 	glPopMatrix();
 }
 
@@ -23,23 +23,23 @@ void Ship::Move(float Seconds)
 {
 	if(m_TurnLeft == true)
 	{
-		m_AngularPosition += m_ShipClass->m_TurnSpeed * Seconds;
+		m_AngularPosition += m_ShipClass->GetTurnSpeed() * Seconds;
 	}
 	if(m_TurnRight == true)
 	{
-		m_AngularPosition -= m_ShipClass->m_TurnSpeed * Seconds;
+		m_AngularPosition -= m_ShipClass->GetTurnSpeed() * Seconds;
 	}
 	m_Position += m_Velocity * Seconds;
 	if(m_Accelerate == true)
 	{
-		math3d::vector2f ForwardThrust(m_ShipClass->m_ForwardThrust, m_AngularPosition, math3d::vector2f::magnitude_angle);
+		math3d::vector2f ForwardThrust(m_ShipClass->GetForwardThrust(), m_AngularPosition, math3d::vector2f::magnitude_angle);
 		
 		m_Position += ForwardThrust * (Seconds * Seconds / 2.0f);
 		m_Velocity += ForwardThrust * Seconds;
-		if(m_Velocity.length() > m_ShipClass->m_MaximumVelocity)
+		if(m_Velocity.length() > m_ShipClass->GetMaximumVelocity())
 		{
 			m_Velocity.normalize();
-			m_Velocity.scale(m_ShipClass->m_MaximumVelocity);
+			m_Velocity.scale(m_ShipClass->GetMaximumVelocity());
 		}
 	}
 }
--- ship_class.h
+++ ship_class.h
@@ -8,20 +8,31 @@ class Model;
 class ShipClass
 {
 public:
+	// constructor
 	ShipClass(const std::string & Identifier);
-	void SetCargoHoldSize(const float & CargoHoldSize);
+	// getters
 	const std::string & GetIdentifier(void) const;
-	const float & GetForwardThrust(void) const;
-	const float & GetTurnSpeed(void) const;
-	const float & GetMaximumVelocity(void) const;
-	const float & GetCargoHoldSize(void) const;
-	Model * m_Model;
+	const Model * GetModel(void) const;
+	float GetForwardThrust(void) const;
+	float GetTurnSpeed(void) const;
+	float GetMaximumVelocity(void) const;
+	float GetCargoHoldSize(void) const;
+	float GetFuelHoldSize(void) const;
+	// modifiers
+	void SetModel(const Model * Model);
+	void SetForwardThrust(float ForwardThrust);
+	void SetTurnSpeed(float TurnSpeed);
+	void SetMaximumVelocity(float MaximumVelocity);
+	void SetCargoHoldSize(float CargoHoldSize);
+	void SetFuelHoldSize(float FuelHoldSize);
+private:
+	std::string m_Identifier;
+	const Model * m_Model;
 	float m_ForwardThrust;
 	float m_TurnSpeed;
 	float m_MaximumVelocity;
-private:
-	std::string m_Identifier;
 	float m_CargoHoldSize;
+	float m_FuelHoldSize;
 };
 
 inline const std::string & ShipClass::GetIdentifier(void) const
@@ -29,24 +40,64 @@ inline const std::string & ShipClass::GetIdentifier(void) const
 	return m_Identifier;
 }
 
-inline const float & ShipClass::GetForwardThrust(void) const
+inline const Model * ShipClass::GetModel(void) const
+{
+	return m_Model;
+}
+
+inline float ShipClass::GetForwardThrust(void) const
 {
 	return m_ForwardThrust;
 }
 
-inline const float & ShipClass::GetTurnSpeed(void) const
+inline float ShipClass::GetTurnSpeed(void) const
 {
 	return m_TurnSpeed;
 }
 
-inline const float & ShipClass::GetMaximumVelocity(void) const
+inline float ShipClass::GetMaximumVelocity(void) const
 {
 	return m_MaximumVelocity;
 }
 
-inline const float & ShipClass::GetCargoHoldSize(void) const
+inline float ShipClass::GetCargoHoldSize(void) const
 {
 	return m_CargoHoldSize;
+}
+
+inline float ShipClass::GetFuelHoldSize(void) const
+{
+	return m_FuelHoldSize;
+}
+
+inline void ShipClass::SetModel(const Model * Model)
+{
+	m_Model = Model;
+}
+
+inline void ShipClass::SetForwardThrust(float ForwardThrust)
+{
+	m_ForwardThrust = ForwardThrust;
+}
+
+inline void ShipClass::SetTurnSpeed(float TurnSpeed)
+{
+	m_TurnSpeed = TurnSpeed;
+}
+
+inline void ShipClass::SetMaximumVelocity(float MaximumVelocity)
+{
+	m_MaximumVelocity = MaximumVelocity;
+}
+
+inline void ShipClass::SetCargoHoldSize(float CargoHoldSize)
+{
+	m_CargoHoldSize = CargoHoldSize;
+}
+
+inline void ShipClass::SetFuelHoldSize(float FuelHoldSize)
+{
+	m_FuelHoldSize = FuelHoldSize;
 }
 
 #endif
