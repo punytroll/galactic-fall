ca671d1f56b8653df294266526ec6f3223cb9167
hagen.moebius <hagen.moebius@localhost>
2007-01-30 18:58:39 +0000

    Made GetExploredSystems() and GetUnexploredSystem() return const references.

--- map_knowledge.h
+++ map_knowledge.h
@@ -9,19 +9,19 @@ class MapKnowledge
 {
 public:
 	void AddExploredSystem(System * ExploredSystem);
-	const std::set< System * > GetExploredSystems(void) const;
-	const std::set< System * > GetUnexploredSystems(void) const;
+	const std::set< System * > & GetExploredSystems(void) const;
+	const std::set< System * > & GetUnexploredSystems(void) const;
 private:
 	std::set< System * > m_ExploredSystems;
 	std::set< System * > m_UnexploredSystems;
 };
 
-inline const std::set< System * > MapKnowledge::GetExploredSystems(void) const
+inline const std::set< System * > & MapKnowledge::GetExploredSystems(void) const
 {
 	return m_ExploredSystems;
 }
 
-inline const std::set< System * > MapKnowledge::GetUnexploredSystems(void) const
+inline const std::set< System * > & MapKnowledge::GetUnexploredSystems(void) const
 {
 	return m_UnexploredSystems;
 }
