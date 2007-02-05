5d7480b1c44bcf53d8be8d1b56d3dcdce60caf15
hagen.moebius <hagen.moebius@localhost>
2007-02-05 02:59:20 +0000

    Added a function GenerateObjectIdentifier to automatically generate object identifiers through a common interface.

--- object.h
+++ object.h
@@ -14,6 +14,7 @@ public:
 	Object(void);
 	virtual ~Object(void);
 	void SetObjectIdentifier(const std::string & ObjectIdentifier);
+	void GenerateObjectIdentifier(void);
 	const std::string & GetObjectIdentifier(void) const;
 	
 	static void Dump(std::ostream & OStream);
