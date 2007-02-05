f910f5ef3ace0f1470567810ac3a30823443fd4e
Hagen Möbius <hagen.moebius@googlemail.com>
2007-02-05 06:52:40 +0000

    Moved the definition of the Shuttle ship class into a separate file and included it into the Arx building process.

--- arx_reading.h
+++ arx_reading.h
@@ -6,12 +6,15 @@ class Item;
 class Label;
 class Model;
 class ModelManager;
+class ShipClass;
+class ShipClassManager;
 class Widget;
 
 Arxx::Item * GetItem(Arxx::Archive & Archive, Arxx::u4byte UniqueIdentifier);
 
 Label * ReadLabel(Arxx::Item * Item);
 Model * ReadModel(ModelManager * ModelManager, Arxx::Item * Item);
+ShipClass * ReadShipClass(ShipClassManager * ShipClassManager, Arxx::Item * Item);
 Widget * ReadWidget(Arxx::Item * Item);
 
 #endif
