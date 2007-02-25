dd4a5fc8a9535516fb9ccc5ae0fbb61c53ae1d3f
hagen.moebius <hagen.moebius@localhost>
2007-02-25 12:36:03 +0000

    When entering a system it will now get populated with up to three ships controlled by the computer. Added some basic State Machine intelligence, so the other ships actually get to move. Put some helper function concerning mathmatics into a new file math.h so they can be accessed not only from main.cpp.

---
+++ state_machine.h
@@ -0,0 +1,18 @@
+#ifndef STATE_MACHINE_H
+#define STATE_MACHINE_H
+
+class State;
+
+class StateMachine
+{
+public:
+	StateMachine(void);
+	void SetGlobalState(State * State);
+	void SetState(State * State);
+	void Update(void);
+private:
+	State * m_State;
+	State * m_GlobalState;
+};
+
+#endif
