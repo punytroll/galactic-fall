ff22b96a9d57c9ce6bb864751535487629d5c130
Hagen Möbius <hagen.moebius@googlemail.com>
2007-05-07 11:52:37 +0000

    Moved to source distribution of string_cast 1.0.1.

--- string_cast.cpp
+++ string_cast.cpp
@@ -1,6 +1,50 @@
+/**
+ * Copyright (C) 2007  Hagen Möbius
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+/**
+ * This is version 1.0.1 of the string cast.
+ **/
+
 #include <sstream>
 
 #include "string_cast.h"
+
+template < >
+int from_string_cast< int >(const std::string & String)
+{
+	std::istringstream StringStream(String);
+	int Result;
+	
+	StringStream >> Result;
+	
+	return Result;
+}
+
+template < >
+unsigned int from_string_cast< unsigned int >(const std::string & String)
+{
+	std::istringstream StringStream(String);
+	unsigned int Result;
+	
+	StringStream >> Result;
+	
+	return Result;
+}
 
 template < >
 unsigned long from_string_cast< unsigned long >(const std::string & String)
@@ -18,6 +62,17 @@ float from_string_cast< float >(const std::string & String)
 {
 	std::istringstream StringStream(String);
 	float Result;
+	
+	StringStream >> Result;
+	
+	return Result;
+}
+
+template < >
+double from_string_cast< double >(const std::string & String)
+{
+	std::istringstream StringStream(String);
+	double Result;
 	
 	StringStream >> Result;
 	
