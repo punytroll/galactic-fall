15e4b7786804f31459ac2f9f6e147d84b7c8dd93
hagen.moebius <hagen.moebius@localhost>
2007-06-04 10:34:19 +0000

    This code adds a mind stack to the character component. Eventually this will render the active and suspended Minds unnecesary. A Character can be possessed by a new mind which pushes the new mind on top of the stack implicitely making the active mind become suspended. When the new Update method is called, it will in turn call Update on the top most mind. NOTE: When a character is deleted it will NOT delete the minds. Instead those a roaming free then. Also added a static set which contains all characters in the curently running game. Characters are inserted into this map by the constructor and erased by the destructor.

--- character.cpp
+++ character.cpp
@@ -1,11 +1,28 @@
 #include "character.h"
 #include "map_knowledge.h"
+#include "mind.h"
+
+std::set< Character * > Character::m_Characters;
 
 Character::Character(void) :
 	m_Credits(0.0f),
 	m_MapKnowledge(new MapKnowledge()),
 	m_Ship(0)
 {
+	m_Characters.insert(this);
+}
+
+Character::~Character(void)
+{
+	m_Characters.erase(m_Characters.find(this));
+}
+
+void Character::Update(void)
+{
+	if(m_Minds.size() > 0)
+	{
+		m_Minds.top()->Update();
+	}
 }
 
 void Character::AddCredits(const float & Credits)
@@ -25,4 +42,28 @@ bool Character::RemoveCredits(const float & Credits)
 		
 		return true;
 	}
+}
+
+void Character::PossessByMind(Mind * Mind)
+{
+	m_Minds.push(Mind);
+}
+
+Mind * Character::ReleaseMind(void)
+{
+	Mind * Result(m_Minds.top());
+	
+	m_Minds.pop();
+	
+	return Result;
+}
+
+CharacterWithShip::CharacterWithShip(const Ship * Ship) :
+	m_Ship(Ship)
+{
+}
+	
+bool CharacterWithShip::operator()(const Character * Character) const
+{
+	return m_Ship == Character->GetShip();
 }
--- character.h
+++ character.h
@@ -1,15 +1,27 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
+#include <set>
+#include <stack>
+
 #include "object.h"
 
 class MapKnowledge;
+class Mind;
 class Ship;
 
 class Character : public Object
 {
+/** static management **/
+public:
+	static std::set< Character * > & GetCharacters(void);
+private:
+	static std::set< Character * > m_Characters;
+
+/** individual **/
 public:
 	Character(void);
+	virtual ~Character(void);
 	// getters
 	float GetCredits(void);
 	MapKnowledge * GetMapKnowledge(void);
@@ -19,13 +31,32 @@ public:
 	void SetCredits(float Credits);
 	void SetShip(Ship * Ship);
 	// modifiers
+	void Update(void);
 	void AddCredits(const float & Credits);
 	bool RemoveCredits(const float & Credits);
+	// mind managemant
+	void PossessByMind(Mind * Mind);
+	Mind * ReleaseMind(void);
 private:
+	std::stack< Mind * > m_Minds;
 	float m_Credits;
 	MapKnowledge * m_MapKnowledge;
 	Ship * m_Ship;
 };
+
+class CharacterWithShip : public std::unary_function< Ship *, bool >
+{
+public:
+	explicit CharacterWithShip(const Ship * Ship);
+	bool operator()(const Character * Character) const;
+private:
+	const Ship * m_Ship;
+};
+
+inline std::set< Character * > & Character::GetCharacters(void)
+{
+	return m_Characters;
+}
 
 inline float Character::GetCredits(void)
 {
