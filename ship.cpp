bc00e379d75ac4f447b0ce5ec07a252c83579827
Hagen Möbius <hagen.moebius@googlemail.com>
2007-06-04 11:48:09 +0000

    Whenperforming the jump action also clear the ship's scanner's target.

--- ship.cpp
+++ ship.cpp
@@ -77,6 +77,7 @@ void Ship::Update(float Seconds)
 			GetLinkedSystemTarget()->AddShip(this);
 			SetCurrentSystem(GetLinkedSystemTarget());
 			SetLinkedSystemTarget(0);
+			SetTarget(0);
 			m_Jump = false;
 		}
 	}
