d7df43bf2c811cb10ee98b581c7878498037055a
Hagen Möbius <hagen.moebius@googlemail.com>
2007-06-04 12:18:39 +0000

    Fixed a mistake with the conditions for jumping.

--- ship.cpp
+++ ship.cpp
@@ -70,7 +70,7 @@ void Ship::Update(float Seconds)
 {
 	if(m_Jump == true)
 	{
-		if((GetShipClass()->GetJumpFuel() > GetFuel()) && (GetLinkedSystemTarget() != 0))
+		if((GetFuel() >= GetShipClass()->GetJumpFuel()) && (GetLinkedSystemTarget() != 0))
 		{
 			GetCurrentSystem()->RemoveShip(this);
 			SetFuel(GetFuel() - GetShipClass()->GetJumpFuel());
