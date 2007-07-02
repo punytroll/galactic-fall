884fcb3737fc86dee924a493fe28862f33bc3602
hagen.moebius <hagen.moebius@localhost>
2007-07-02 16:19:15 +0000

    Added quaternion reading function.

--- buffer_reading.h
+++ buffer_reading.h
@@ -23,10 +23,12 @@
 #include <BufferReader.h>
 
 #include "color.h"
+#include "math/quaternion.h"
 #include "math/vector2f.h"
 #include "math/vector3f.h"
 
 Arxx::BufferReader & operator>>(Arxx::BufferReader & BufferReader, Color & Color);
+Arxx::BufferReader & operator>>(Arxx::BufferReader & BufferReader, Quaternion & Quaternion);
 Arxx::BufferReader & operator>>(Arxx::BufferReader & BufferReader, Vector2f & Vector);
 Arxx::BufferReader & operator>>(Arxx::BufferReader & BufferReader, Vector3f & Vector);
 
