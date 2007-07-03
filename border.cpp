f04cccbf6878f8c1381721952fbb00ae975a4ef7
hagen.moebius <hagen.moebius@localhost>
2007-07-03 15:01:17 +0000

    Fixed the year of the copyright statement.

--- border.cpp
+++ border.cpp
@@ -1,6 +1,6 @@
 /**
  * galactic-fall
- * Copyright (C) 2006  Hagen Möbius
+ * Copyright (C) 2007  Hagen Möbius
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
