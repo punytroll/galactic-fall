3d7f0f84a2c034dcd432b3f1d67f2e03f1afbb09
Hagen Möbius <hagen.moebius@googlemail.com>
2007-08-10 13:21:57 +0000

    Added a dialog which displays timing information. The dialog can be shown and hidden with F12. The dialog displays FramesPerSecond, TotalSecondsPerFrame, AISecondsPerFrame, GraphicsSecondsPerFrame, PhysicsSecondsPerFrame and TotalSecondsPerFrame. The display is very fast updating so you don't really see anything. I will add a 10 frame sliding window average.

---
+++ source/timing_dialog.h
@@ -0,0 +1,44 @@
+/**
+ * galactic-fall
+ * Copyright (C) 2007  Hagen Möbius
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef TIMING_DIALOG_H
+#define TIMING_DIALOG_H
+
+#include "window.h"
+
+class Label;
+
+class TimingDialog : public WWindow
+{
+public:
+	TimingDialog(Widget * SupWidget);
+	void UpdateFramesPerSecond(float FramesPerSecond);
+	void UpdateAISecondsPerFrame(float AISecondsPerFrame);
+	void UpdateGraphicsSecondsPerFrame(float GraphicsSecondsPerFrame);
+	void UpdatePhysicsSecondsPerFrame(float PhysicsSecondsPerFrame);
+	void UpdateTotalSecondsPerFrame(float TotalSecondsPerFrame);
+private:
+	Label * m_FramesPerSecondLabel;
+	Label * m_AISecondsPerFrameLabel;
+	Label * m_GraphicsSecondsPerFrameLabel;
+	Label * m_PhysicsSecondsPerFrameLabel;
+	Label * m_TotalSecondsPerFrameLabel;
+};
+
+#endif
