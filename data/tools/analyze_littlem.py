acaa6a8f8bb416c358201810753f27563d267d80
Hagen Möbius <hagen.moebius@googlemail.com>
2007-08-24 21:54:31 +0000

    Added a little tool which analyzes a given littlem model file and prints the number of points, triangle-points and triangles.

---
+++ data/tools/analyze_littlem.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+from optparse import OptionParser
+from sys import exit
+from xml.dom.minidom import Node, parse
+
+parser = OptionParser()
+parser.add_option("-i", "--in", dest="in_file", help="The littlem file to analyze.")
+
+# read the options and validate
+(options, args) = parser.parse_args()
+if options.in_file == None:
+	print "Set the raw file to convert with '--in'."
+	exit(1)
+
+def analyze(model_element):
+	print "identifier: " + model_element.attributes.get("identifier").nodeValue
+	number_of_points = 0
+	number_of_triangle_points = 0
+	number_of_triangles = 0
+	for element in model_element.childNodes:
+		if element.nodeType == Node.ELEMENT_NODE:
+			if element.tagName == "point":
+				number_of_points += 1
+			elif element.tagName == "triangle-point":
+				number_of_triangle_points += 1
+			elif element.tagName == "triangle":
+				number_of_triangles += 1
+	print "#points: " + str(number_of_points)
+	print "#triangle-points: " + str(number_of_triangle_points)
+	print "#triangles: " + str(number_of_triangles)
+
+# now parse the in_file
+in_document = parse(options.in_file)
+model_element = in_document.documentElement
+if model_element.nodeType == Node.ELEMENT_NODE:
+	analyze(model_element)
