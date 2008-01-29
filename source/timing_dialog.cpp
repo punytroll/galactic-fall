32b11c98d6deb61c9bff4809ebd991b1ebcdc63b
Hagen Möbius <hagen.moebius@googlemail.com>
2008-01-29 18:16:41 +0000

    The former metrix TotalSecondsPerFrame was not correct, as it measured only the time in the GameFrame() function. Therefore I renamed it to TotalSecondsPerFrameProcessing. Added another metric to measure the time that the programm runs between two calls to GameFrame(). This is the real time a frame needs to be performed and thus it is correctly named TotalSecondsPerFrame. The FPS is now calculated using the new TotalSecondsPerFrame metric. This fixes issue #7.

--- source/timing_dialog.cpp
+++ source/timing_dialog.cpp
@@ -25,50 +25,59 @@ TimingDialog::TimingDialog(Widget * SupWidget) :
 	WWindow(SupWidget, "Timing Information")
 {
 	SetPosition(Vector2f(300.0f, 300.0f));
-	SetSize(Vector2f(250.0f, 300.0f));
+	SetSize(Vector2f(320.0f, 300.0f));
 	
 	Label * FramesPerSecondCaptionLabel(new Label(this, "Frames per Second:"));
 	
 	FramesPerSecondCaptionLabel->SetPosition(Vector2f(10.0f, 40.0f));
-	FramesPerSecondCaptionLabel->SetSize(Vector2f(150.0f, 20.0f));
+	FramesPerSecondCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
 	m_FramesPerSecondLabel = new Label(this, "0");
-	m_FramesPerSecondLabel->SetPosition(Vector2f(170.0f, 40.0f));
+	m_FramesPerSecondLabel->SetPosition(Vector2f(240.0f, 40.0f));
 	m_FramesPerSecondLabel->SetSize(Vector2f(70.0f, 20.0f));
 	m_FramesPerSecondLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * TotalSecondsPerFrameCaptionLabel(new Label(this, "Total Seconds per Frame:"));
 	
 	TotalSecondsPerFrameCaptionLabel->SetPosition(Vector2f(10.0f, 60.0f));
-	TotalSecondsPerFrameCaptionLabel->SetSize(Vector2f(150.0f, 20.0f));
+	TotalSecondsPerFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
 	m_TotalSecondsPerFrameLabel = new Label(this, "0");
-	m_TotalSecondsPerFrameLabel->SetPosition(Vector2f(170.0f, 60.0f));
+	m_TotalSecondsPerFrameLabel->SetPosition(Vector2f(240.0f, 60.0f));
 	m_TotalSecondsPerFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
 	m_TotalSecondsPerFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
+	Label * TotalSecondsPerFrameProcessingCaptionLabel(new Label(this, "Total Seconds per Frame Processing:"));
+	
+	TotalSecondsPerFrameProcessingCaptionLabel->SetPosition(Vector2f(10.0f, 80.0f));
+	TotalSecondsPerFrameProcessingCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	m_TotalSecondsPerFrameProcessingLabel = new Label(this, "0");
+	m_TotalSecondsPerFrameProcessingLabel->SetPosition(Vector2f(240.0f, 80.0f));
+	m_TotalSecondsPerFrameProcessingLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_TotalSecondsPerFrameProcessingLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
+	
 	Label * AISecondsPerFrameCaptionLabel(new Label(this, "AI Seconds per Frame:"));
 	
-	AISecondsPerFrameCaptionLabel->SetPosition(Vector2f(10.0f, 80.0f));
-	AISecondsPerFrameCaptionLabel->SetSize(Vector2f(150.0f, 20.0f));
+	AISecondsPerFrameCaptionLabel->SetPosition(Vector2f(10.0f, 100.0f));
+	AISecondsPerFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
 	m_AISecondsPerFrameLabel = new Label(this, "0");
-	m_AISecondsPerFrameLabel->SetPosition(Vector2f(170.0f, 80.0f));
+	m_AISecondsPerFrameLabel->SetPosition(Vector2f(240.0f, 100.0f));
 	m_AISecondsPerFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
 	m_AISecondsPerFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * PhysicsSecondsPerFrameCaptionLabel(new Label(this, "Physics Seconds per Frame:"));
 	
-	PhysicsSecondsPerFrameCaptionLabel->SetPosition(Vector2f(10.0f, 100.0f));
-	PhysicsSecondsPerFrameCaptionLabel->SetSize(Vector2f(150.0f, 20.0f));
+	PhysicsSecondsPerFrameCaptionLabel->SetPosition(Vector2f(10.0f, 120.0f));
+	PhysicsSecondsPerFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
 	m_PhysicsSecondsPerFrameLabel = new Label(this, "0");
-	m_PhysicsSecondsPerFrameLabel->SetPosition(Vector2f(170.0f, 100.0f));
+	m_PhysicsSecondsPerFrameLabel->SetPosition(Vector2f(240.0f, 120.0f));
 	m_PhysicsSecondsPerFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
 	m_PhysicsSecondsPerFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * GraphicsSecondsPerFrameCaptionLabel(new Label(this, "Graphics Seconds per Frame:"));
 	
-	GraphicsSecondsPerFrameCaptionLabel->SetPosition(Vector2f(10.0f, 120.0f));
-	GraphicsSecondsPerFrameCaptionLabel->SetSize(Vector2f(150.0f, 20.0f));
+	GraphicsSecondsPerFrameCaptionLabel->SetPosition(Vector2f(10.0f, 140.0f));
+	GraphicsSecondsPerFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
 	m_GraphicsSecondsPerFrameLabel = new Label(this, "0");
-	m_GraphicsSecondsPerFrameLabel->SetPosition(Vector2f(170.0f, 120.0f));
+	m_GraphicsSecondsPerFrameLabel->SetPosition(Vector2f(240.0f, 140.0f));
 	m_GraphicsSecondsPerFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
 	m_GraphicsSecondsPerFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 }
@@ -96,4 +105,9 @@ void TimingDialog::UpdatePhysicsSecondsPerFrame(float PhysicsSecondsPerFrame)
 void TimingDialog::UpdateTotalSecondsPerFrame(float TotalSecondsPerFrame)
 {
 	m_TotalSecondsPerFrameLabel->SetString(to_string_cast(TotalSecondsPerFrame * 1000, 2) + " ms");
+}
+
+void TimingDialog::UpdateTotalSecondsPerFrameProcessing(float TotalSecondsPerFrameProcessing)
+{
+	m_TotalSecondsPerFrameProcessingLabel->SetString(to_string_cast(TotalSecondsPerFrameProcessing * 1000, 2) + " ms");
 }
