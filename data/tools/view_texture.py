bd2dbf7293e5bf8464ca763e53682c796a39778a
Hagen Möbius <hagen.moebius@googlemail.com>
2008-02-07 12:19:28 +0000

    I have completely rewritten the view_texture tool. It is now implemented without PyQt straight on top of PyOpenGL with glut. This doesn't make it any faster but it removes one dependency from the project.

--- data/tools/view_texture.py
+++ data/tools/view_texture.py
@@ -1,108 +1,92 @@
-#! /usr/bin/python
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import struct
 
-from optparse import OptionParser
-from PyQt4 import QtCore, QtGui, QtOpenGL
-from struct import unpack
-import sys
+window = None
+texture = None
+x = None
+y = None
 
-parser = OptionParser()
-parser.add_option("-i", "--in", dest="in_file", help="The raw image file with the texture information.")
-(options, args) = parser.parse_args()
-if options.in_file == None:
-	print "Set the file containing the texture with '--in'."
-	sys.exit(1)
+def load_texture(file_name):
+	global texture, x, y
+	texture = glGenTextures(1)
+	glBindTexture(GL_TEXTURE_2D, texture)
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+	file = open(file_name, "rb")
+	(dimension_x, dimension_y, format) = struct.unpack("!III", file.read(12))
+	if dimension_x > dimension_y:
+		x = 0.5
+		y = 0.5 * (float(dimension_y) / float(dimension_x))
+	else:
+		x = 0.5 * (float(dimension_x) / float(dimension_y))
+		y = 0.5
+	print "Reading texture with width='" + str(dimension_x) + "' and height='" + str(dimension_y) + "'."
+	if format == 1: # RGBA one byte each
+		print "Reading in format='RGBA one byte each'."
+		pixels = file.read(4 * dimension_x * dimension_y)
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, dimension_x, dimension_y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels)
+	elif format == 2: # RGB one byte each
+		print "Reading in format='RGB one byte each'."
+		pixels = file.read(3 * dimension_x * dimension_y)
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, dimension_x, dimension_y, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels)
+	else:
+		print "Unknown format '" + str(format) + "'."
+		sys.exit(1)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
-# now reading the file content
-in_file = open(options.in_file, "rb")
-(dimension_x, dimension_y, format) = unpack("!III", in_file.read(12))
-print "Reading texture with width='" + str(dimension_x) + "' and height='" + str(dimension_y) + "'."
-if format == 1: # RGBA one byte each
-	print "Reading in format='RGBA one byte each'."
-	pixels = in_file.read(4 * dimension_x * dimension_y)
-elif format == 2: # RGB one byte each
-	print "Reading in format='RGB one byte each'."
-	pixels = in_file.read(3 * dimension_x * dimension_y)
-else:
-	print "Unknown format '" + str(format) + "'."
-	sys.exit(1)
-aspect = float(dimension_x) / float(dimension_y)
-if aspect > 1:
-	x = 1.0
-	y = 1.0 / aspect
-else:
-	x = 1.0 * aspect
-	y = 1.0
+def initialize_opengl(Width, Height):
+	load_texture(sys.argv[1])
+	glEnable(GL_TEXTURE_2D)
+	glClearColor(0.0, 0.0, 0.0, 0.0)
+	glClearDepth(1.0)
+	glEnable(GL_BLEND)
+	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
 
-try:
-    from OpenGL import GL
-except ImportError:
-	print"PyOpenGL must be installed to run the texture viewer."
-	sys.exit(1)
+def resize_window(Width, Height):
+	if Height == 0:
+		Height = 1
+	glViewport(0, 0, Width, Height)
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	gluPerspective(30.0, float(Width)/float(Height), 0.1, 100.0)
+	glMatrixMode(GL_MODELVIEW)
 
-class GLWidget(QtOpenGL.QGLWidget):
-	def __init__(self, parent=None):
-		QtOpenGL.QGLWidget.__init__(self, parent)
-		self.setMinimumSize(400, 400)
+def draw_scene():
+	global texture, x, y
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glLoadIdentity()
+	glTranslatef(0.0, 0.0, -1.6)
+	glBindTexture(GL_TEXTURE_2D,texture)
+	glBegin(GL_QUADS)
+	glTexCoord2f(0.0, 0.0)
+	glVertex3f(-x, -y, 0.0)
+	glTexCoord2f(1.0, 0.0)
+	glVertex3f(x, -y, 0.0)
+	glTexCoord2f(1.0, 1.0)
+	glVertex3f(x, y, 0.0)
+	glTexCoord2f(0.0, 1.0)
+	glVertex3f(-x, y, 0.0)
+	glEnd()
+	glutSwapBuffers()
 
-	def initializeGL(self):
-		GL.glClearColor(0.0, 0.0, 0.0, 0.0)
-		GL.glShadeModel(GL.GL_FLAT)
-		GL.glEnable(GL.GL_DEPTH_TEST)
-		GL.glEnable(GL.GL_CULL_FACE)
-		GL.glEnable(GL.GL_TEXTURE_2D)
-		#~ GL.glEnable(GL.GL_BLEND)
-		GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE);
-		self.__texture = GL.glGenTextures(1)
-		GL.glBindTexture(GL.GL_TEXTURE_2D, self.__texture)
-		if format == 1:
-			GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, dimension_x, dimension_y, 0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, pixels)
-		elif format == 2:
-			GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, dimension_x, dimension_y, 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, pixels)
-		else:
-			print "Don't know how to pass the image data to OpenGL."
-			sys.exit(1)
-		GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR)
-		GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR)
+def key_pressed(key, x, y):
+	if key == '\033':
+		glutDestroyWindow(window)
+		sys.exit()
 
-	def paintGL(self):
-		GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
-		GL.glLoadIdentity()
-		GL.glTranslated(0.0, 0.0, -10.0)
-		GL.glBindTexture(GL.GL_TEXTURE_2D, self.__texture)
-		GL.glBegin(GL.GL_QUADS)
-		GL.glTexCoord2f(0.0, 0.0)
-		GL.glVertex(-x, y, 0.0)
-		GL.glTexCoord2f(1.0, 0.0)
-		GL.glVertex(x, y, 0.0)
-		GL.glTexCoord2f(1.0, 1.0)
-		GL.glVertex(x, -y, 0.0)
-		GL.glTexCoord2f(0.0, 1.0)
-		GL.glVertex(-x, -y, 0.0)
-		GL.glEnd()
+def main():
+	global window
+	glutInit("")
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+	glutInitWindowSize(640, 480)
+	glutInitWindowPosition(0, 0)
+	window = glutCreateWindow("Texture Viewer")
+	glutDisplayFunc(draw_scene)
+	glutReshapeFunc(resize_window)
+	glutKeyboardFunc(key_pressed)
+	initialize_opengl(640, 480)
+	glutMainLoop()
 
-	def resizeGL(self, width, height):
-		side = min(width, height)
-		GL.glViewport((width - side) / 2, (height - side) / 2, side, side)
-		GL.glMatrixMode(GL.GL_PROJECTION)
-		GL.glLoadIdentity()
-		GL.glOrtho(-1.2, +1.2, +1.2, -1.2, 4.0, 15.0)
-		GL.glMatrixMode(GL.GL_MODELVIEW)
-
-class Window(QtGui.QWidget):
-	def __init__(self, parent = None):
-		QtGui.QWidget.__init__(self, parent)
-		self.__opengl_widget = GLWidget()
-		main_layout = QtGui.QHBoxLayout()
-		main_layout.addWidget(self.__opengl_widget)
-		self.setLayout(main_layout)
-		self.setWindowTitle("Texture Viewer")
-	
-	def keyPressEvent(self, event):
-		if event.key() == QtCore.Qt.Key_Escape:
-			sys.exit(0)
-
-if __name__ == '__main__':
-	application = QtGui.QApplication(sys.argv)
-	window = Window()
-	window.showMaximized()
-	sys.exit(application.exec_())
+main()
