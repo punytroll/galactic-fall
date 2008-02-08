2e73a36062e6f9ae35f03c5068b8fc2e268e5623
hagen.moebius <hagen.moebius@localhost>
2008-02-08 11:34:34 +0000

    Added the three metrics CommoditiesInCurrentSystem, ShipsInCurrentSystem and ShotsInCurrentSystem to the system statistics and timing dialog. They are ecalculated during CalculateMovements in main.cpp. Added some anchors and parent-related positions to the timing dialog to allow better resizing. This closes issue 17.

--- source/timing_dialog.cpp
+++ source/timing_dialog.cpp
@@ -27,84 +27,145 @@ TimingDialog::TimingDialog(Widget * SupWidget) :
 	WWindow(SupWidget, "Timing Information")
 {
 	SetPosition(Vector2f(300.0f, 300.0f));
-	SetSize(Vector2f(320.0f, 300.0f));
+	SetSize(Vector2f(350.0f, 300.0f));
 	
 	Label * FramesPerSecondCaptionLabel(new Label(this, "Frames per Second:"));
 	
 	FramesPerSecondCaptionLabel->SetPosition(Vector2f(10.0f, 40.0f));
-	FramesPerSecondCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	FramesPerSecondCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	FramesPerSecondCaptionLabel->SetAnchorRight(true);
 	m_FramesPerSecondLabel = new Label(this, "0");
-	m_FramesPerSecondLabel->SetPosition(Vector2f(240.0f, 40.0f));
+	m_FramesPerSecondLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 40.0f));
 	m_FramesPerSecondLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_FramesPerSecondLabel->SetAnchorLeft(false);
+	m_FramesPerSecondLabel->SetAnchorRight(true);
 	m_FramesPerSecondLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * FrameToFrameSecondsThisFrameCaptionLabel(new Label(this, "Seconds from Frame to Frame:"));
 	
 	FrameToFrameSecondsThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 60.0f));
-	FrameToFrameSecondsThisFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	FrameToFrameSecondsThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	FrameToFrameSecondsThisFrameCaptionLabel->SetAnchorRight(true);
 	m_FrameToFrameSecondsThisFrameLabel = new Label(this, "0");
-	m_FrameToFrameSecondsThisFrameLabel->SetPosition(Vector2f(240.0f, 60.0f));
+	m_FrameToFrameSecondsThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 60.0f));
 	m_FrameToFrameSecondsThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_FrameToFrameSecondsThisFrameLabel->SetAnchorLeft(false);
+	m_FrameToFrameSecondsThisFrameLabel->SetAnchorRight(true);
 	m_FrameToFrameSecondsThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * ProcessingSecondsThisFrameCaptionLabel(new Label(this, "Processing Seconds this Frame:"));
 	
 	ProcessingSecondsThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 80.0f));
-	ProcessingSecondsThisFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	ProcessingSecondsThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	ProcessingSecondsThisFrameCaptionLabel->SetAnchorRight(true);
 	m_ProcessingSecondsThisFrameLabel = new Label(this, "0");
-	m_ProcessingSecondsThisFrameLabel->SetPosition(Vector2f(240.0f, 80.0f));
+	m_ProcessingSecondsThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 80.0f));
 	m_ProcessingSecondsThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_ProcessingSecondsThisFrameLabel->SetAnchorLeft(false);
+	m_ProcessingSecondsThisFrameLabel->SetAnchorRight(true);
 	m_ProcessingSecondsThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * AISecondsThisFrameCaptionLabel(new Label(this, "AI Seconds this Frame:"));
 	
 	AISecondsThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 100.0f));
-	AISecondsThisFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	AISecondsThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	AISecondsThisFrameCaptionLabel->SetAnchorRight(true);
 	m_AISecondsThisFrameLabel = new Label(this, "0");
-	m_AISecondsThisFrameLabel->SetPosition(Vector2f(240.0f, 100.0f));
+	m_AISecondsThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 100.0f));
 	m_AISecondsThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_AISecondsThisFrameLabel->SetAnchorLeft(false);
+	m_AISecondsThisFrameLabel->SetAnchorRight(true);
 	m_AISecondsThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * PhysicsSecondsThisFrameCaptionLabel(new Label(this, "Physics Seconds this Frame:"));
 	
 	PhysicsSecondsThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 120.0f));
-	PhysicsSecondsThisFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	PhysicsSecondsThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	PhysicsSecondsThisFrameCaptionLabel->SetAnchorRight(true);
 	m_PhysicsSecondsThisFrameLabel = new Label(this, "0");
-	m_PhysicsSecondsThisFrameLabel->SetPosition(Vector2f(240.0f, 120.0f));
+	m_PhysicsSecondsThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 120.0f));
 	m_PhysicsSecondsThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_PhysicsSecondsThisFrameLabel->SetAnchorLeft(false);
+	m_PhysicsSecondsThisFrameLabel->SetAnchorRight(true);
 	m_PhysicsSecondsThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * GraphicsSecondsThisFrameCaptionLabel(new Label(this, "Graphics Seconds this Frame:"));
 	
 	GraphicsSecondsThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 140.0f));
-	GraphicsSecondsThisFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	GraphicsSecondsThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	GraphicsSecondsThisFrameCaptionLabel->SetAnchorRight(true);
 	m_GraphicsSecondsThisFrameLabel = new Label(this, "0");
-	m_GraphicsSecondsThisFrameLabel->SetPosition(Vector2f(240.0f, 140.0f));
+	m_GraphicsSecondsThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 140.0f));
 	m_GraphicsSecondsThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_GraphicsSecondsThisFrameLabel->SetAnchorLeft(false);
+	m_GraphicsSecondsThisFrameLabel->SetAnchorRight(true);
 	m_GraphicsSecondsThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * MessagingSecondsThisFrameCaptionLabel(new Label(this, "Messaging Seconds this Frame:"));
 	
 	MessagingSecondsThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 160.0f));
-	MessagingSecondsThisFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	MessagingSecondsThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	MessagingSecondsThisFrameCaptionLabel->SetAnchorRight(true);
 	m_MessagingSecondsThisFrameLabel = new Label(this, "0");
-	m_MessagingSecondsThisFrameLabel->SetPosition(Vector2f(240.0f, 160.0f));
+	m_MessagingSecondsThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 160.0f));
 	m_MessagingSecondsThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_MessagingSecondsThisFrameLabel->SetAnchorLeft(false);
+	m_MessagingSecondsThisFrameLabel->SetAnchorRight(true);
 	m_MessagingSecondsThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 	
 	Label * DispatchedMessagesThisFrameCaptionLabel(new Label(this, "Dispatched messages this Frame:"));
 	
 	DispatchedMessagesThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 180.0f));
-	DispatchedMessagesThisFrameCaptionLabel->SetSize(Vector2f(220.0f, 20.0f));
+	DispatchedMessagesThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	DispatchedMessagesThisFrameCaptionLabel->SetAnchorRight(true);
 	m_DispatchedMessagesThisFrameLabel = new Label(this, "0");
-	m_DispatchedMessagesThisFrameLabel->SetPosition(Vector2f(240.0f, 180.0f));
+	m_DispatchedMessagesThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 180.0f));
 	m_DispatchedMessagesThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_DispatchedMessagesThisFrameLabel->SetAnchorLeft(false);
+	m_DispatchedMessagesThisFrameLabel->SetAnchorRight(true);
 	m_DispatchedMessagesThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
+	
+	Label * CommoditiesInCurrentSystemThisFrameCaptionLabel(new Label(this, "Commodities in current System this Frame:"));
+	
+	CommoditiesInCurrentSystemThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 200.0f));
+	CommoditiesInCurrentSystemThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	CommoditiesInCurrentSystemThisFrameCaptionLabel->SetAnchorRight(true);
+	m_CommoditiesInCurrentSystemThisFrameLabel = new Label(this, "0");
+	m_CommoditiesInCurrentSystemThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 200.0f));
+	m_CommoditiesInCurrentSystemThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_CommoditiesInCurrentSystemThisFrameLabel->SetAnchorLeft(false);
+	m_CommoditiesInCurrentSystemThisFrameLabel->SetAnchorRight(true);
+	m_CommoditiesInCurrentSystemThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
+	
+	Label * ShipsInCurrentSystemThisFrameCaptionLabel(new Label(this, "Ships in current System this Frame:"));
+	
+	ShipsInCurrentSystemThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 220.0f));
+	ShipsInCurrentSystemThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	ShipsInCurrentSystemThisFrameCaptionLabel->SetAnchorRight(true);
+	m_ShipsInCurrentSystemThisFrameLabel = new Label(this, "0");
+	m_ShipsInCurrentSystemThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 220.0f));
+	m_ShipsInCurrentSystemThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_ShipsInCurrentSystemThisFrameLabel->SetAnchorLeft(false);
+	m_ShipsInCurrentSystemThisFrameLabel->SetAnchorRight(true);
+	m_ShipsInCurrentSystemThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
+	
+	Label * ShotsInCurrentSystemThisFrameCaptionLabel(new Label(this, "Shots in current System this Frame:"));
+	
+	ShotsInCurrentSystemThisFrameCaptionLabel->SetPosition(Vector2f(10.0f, 240.0f));
+	ShotsInCurrentSystemThisFrameCaptionLabel->SetSize(Vector2f(GetSize()[0] - 100.0f, 20.0f));
+	ShotsInCurrentSystemThisFrameCaptionLabel->SetAnchorRight(true);
+	m_ShotsInCurrentSystemThisFrameLabel = new Label(this, "0");
+	m_ShotsInCurrentSystemThisFrameLabel->SetPosition(Vector2f(GetSize()[0] - 80.0f, 240.0f));
+	m_ShotsInCurrentSystemThisFrameLabel->SetSize(Vector2f(70.0f, 20.0f));
+	m_ShotsInCurrentSystemThisFrameLabel->SetAnchorLeft(false);
+	m_ShotsInCurrentSystemThisFrameLabel->SetAnchorRight(true);
+	m_ShotsInCurrentSystemThisFrameLabel->SetHorizontalAlignment(Label::ALIGN_RIGHT);
 }
 
 void TimingDialog::Update(void)
 {
 	m_AISecondsThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetAISecondsThisFrame() * 1000, 2) + " ms");
+	m_CommoditiesInCurrentSystemThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetCommoditiesInCurrentSystemThisFrame()));
 	m_DispatchedMessagesThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetDispatchedMessagesThisFrame()));
 	m_FramesPerSecondLabel->SetString(to_string_cast(g_SystemStatistics->GetFramesPerSecond(), 2));
 	m_FrameToFrameSecondsThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetFrameToFrameSecondsThisFrame() * 1000, 2) + " ms");
@@ -112,4 +173,6 @@ void TimingDialog::Update(void)
 	m_MessagingSecondsThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetMessagingSecondsThisFrame() * 1000, 2) + " ms");
 	m_PhysicsSecondsThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetPhysicsSecondsThisFrame() * 1000, 2) + " ms");
 	m_ProcessingSecondsThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetProcessingSecondsThisFrame() * 1000, 2) + " ms");
+	m_ShipsInCurrentSystemThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetShipsInCurrentSystemThisFrame()));
+	m_ShotsInCurrentSystemThisFrameLabel->SetString(to_string_cast(g_SystemStatistics->GetShotsInCurrentSystemThisFrame()));
 }
