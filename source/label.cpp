511cfdc3c975c45cee54232689e6d8050d6928ac
Hagen Möbius <hagen.moebius@googlemail.com>
2008-03-27 02:43:55 +0000

    Cleaned up the Label class. Renamed String to Text and ForgroundColor to TextColor. This change reaches back into the data.

--- source/label.cpp
+++ source/label.cpp
@@ -24,59 +24,27 @@
 #include "draw_text.h"
 #include "label.h"
 
-Label::Label(Widget * SupWidget) :
+Label::Label(Widget * SupWidget, const std::string & Text) :
 	Widget(SupWidget),
-	m_ForegroundColor(1.0f, 1.0f, 1.0f, 1.0f),
-	m_Wrap(false),
-	m_WordWrap(false),
 	m_HorizontalAlignment(Label::ALIGN_LEFT),
-	m_VerticalAlignment(Label::ALIGN_TOP)
-{
-}
-
-Label::Label(Widget * SupWidget, const std::string & String) :
-	Widget(SupWidget),
-	m_String(String),
-	m_ForegroundColor(1.0f, 1.0f, 1.0f, 1.0f),
+	m_Text(Text),
+	m_TextColor(0),
+	m_VerticalAlignment(Label::ALIGN_TOP),
 	m_Wrap(false),
-	m_WordWrap(false),
-	m_HorizontalAlignment(Label::ALIGN_LEFT),
-	m_VerticalAlignment(Label::ALIGN_TOP)
+	m_WordWrap(false)
 {
+	m_TextColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
 Label::~Label(void)
 {
+	delete m_TextColor;
 }
 
-void Label::SetString(const std::string & String)
+void Label::SetTextColor(const Color & TextColor)
 {
-	m_String = String;
-}
-
-void Label::SetForegroundColor(const Color & ForegroundColor)
-{
-	m_ForegroundColor = ForegroundColor;
-}
-
-void Label::SetWrap(bool Wrap)
-{
-	m_Wrap = Wrap;
-}
-
-void Label::SetWordWrap(bool WordWrap)
-{
-	m_WordWrap = WordWrap;
-}
-
-void Label::SetHorizontalAlignment(const Label::HorizontalAlignment & HorizontalAlignment)
-{
-	m_HorizontalAlignment = HorizontalAlignment;
-}
-
-void Label::SetVerticalAlignment(const Label::VerticalAlignment & VerticalAlignment)
-{
-	m_VerticalAlignment = VerticalAlignment;
+	delete m_TextColor;
+	m_TextColor = new Color(TextColor);
 }
 
 void Label::Draw(void) const
@@ -84,12 +52,12 @@ void Label::Draw(void) const
 	float Width(GetSize().m_V.m_A[0]);
 	
 	Widget::Draw();
-	glColor4fv(m_ForegroundColor.GetColor().m_V.m_A);
+	glColor4fv(m_TextColor->GetColor().m_V.m_A);
 	if(m_Wrap == false)
 	{
 		glPushMatrix();
-		glTranslatef(((m_HorizontalAlignment == Label::ALIGN_LEFT) ? (0.0f) : ((m_HorizontalAlignment == Label::ALIGN_RIGHT) ? (Width - 6.0f * m_String.length()) : (0.5f * (Width - 6.0f * m_String.length())))), ((m_VerticalAlignment == Label::ALIGN_TOP) ? (0.0f) : ((m_VerticalAlignment == Label::ALIGN_BOTTOM) ? (GetSize().m_V.m_A[1] - 12.0f) : (0.5f * (GetSize().m_V.m_A[1] - 12.0f)))), 0.0f);
-		DrawText(m_String);
+		glTranslatef(((m_HorizontalAlignment == Label::ALIGN_LEFT) ? (0.0f) : ((m_HorizontalAlignment == Label::ALIGN_RIGHT) ? (Width - 6.0f * m_Text.length()) : (0.5f * (Width - 6.0f * m_Text.length())))), ((m_VerticalAlignment == Label::ALIGN_TOP) ? (0.0f) : ((m_VerticalAlignment == Label::ALIGN_BOTTOM) ? (GetSize().m_V.m_A[1] - 12.0f) : (0.5f * (GetSize().m_V.m_A[1] - 12.0f)))), 0.0f);
+		DrawText(m_Text);
 		glPopMatrix();
 	}
 	else
@@ -99,14 +67,14 @@ void Label::Draw(void) const
 		std::string::size_type Start(0);
 		std::vector< std::pair< std::string::size_type, std::string::size_type > > Lines;
 		
-		while(Start < m_String.length())
+		while(Start < m_Text.length())
 		{
 			// set the length of the line string to the label width and count downwards
 			Length = LabelWidthInCharacters;
 			// only look for a separator if we want to wrap at word boundaries and this is not the last line, else just wrap
-			if((m_WordWrap == true) && (Start + Length < m_String.length()))
+			if((m_WordWrap == true) && (Start + Length < m_Text.length()))
 			{
-				while((Length > 0) && (m_String[Start + Length] != ' '))
+				while((Length > 0) && (m_Text[Start + Length] != ' '))
 				{
 					--Length;
 				}
@@ -116,14 +84,14 @@ void Label::Draw(void) const
 			{
 				Length = LabelWidthInCharacters;
 			}
-			Lines.push_back(std::make_pair(Start, std::min(m_String.length() - Start, Length)));
+			Lines.push_back(std::make_pair(Start, std::min(m_Text.length() - Start, Length)));
 			Start += Length + 1;
 		}
 		for(std::vector< std::pair< std::string::size_type, std::string::size_type > >::size_type Line = 0; Line < Lines.size(); ++Line)
 		{
 			glPushMatrix();
 			glTranslatef(((m_HorizontalAlignment == Label::ALIGN_LEFT) ? (0.0f) : ((m_HorizontalAlignment == Label::ALIGN_RIGHT) ? (Width - 6.0f * Lines[Line].second) : ((Width - 6.0f * Lines[Line].second) / 2.0f))),  ((m_VerticalAlignment == Label::ALIGN_TOP) ? (12.0f * Line) : ((m_VerticalAlignment == Label::ALIGN_BOTTOM) ? (GetSize().m_V.m_A[1] - 12.0f * Line) : (0.5f * (GetSize().m_V.m_A[1] - 12.0f * Lines.size()) + 12.0f * Line))), 0.0f);
-			DrawText(m_String.substr(Lines[Line].first, Lines[Line].second));
+			DrawText(m_Text.substr(Lines[Line].first, Lines[Line].second));
 			glPopMatrix();
 		}
 	}
