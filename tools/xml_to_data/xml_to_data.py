3714351e839c4e22230379f15652cab1f0a93607
hagen.moebius <hagen.moebius@localhost>
2009-03-30 09:47:33 +0000

    Put some declarations properties into a class so they are better expandable.

--- tools/xml_to_data/xml_to_data.py
+++ tools/xml_to_data/xml_to_data.py
@@ -37,21 +37,85 @@ if options.out_file == None:
 	print LightRed + "Usage" + White + ": Set the file to write the binary form to with " + LightYellow + "--out" + White + "."
 	exit(1)
 
+class Declaration(object):
+	def __init__(self, identifier):
+		self.__identifier = identifier
+		self.__is_declaration = None
+		self.__parts = list()
+		self.__sub_type = None
+		self.__type = None
+	
+	def __get_identifier(self):
+		return self.__identifier
+	
+	def __get_is_declaration(self):
+		return self.__is_declaration
+	
+	def __get_parts(self):
+		return self.__parts
+	
+	def __get_sub_type(self):
+		return self.__sub_type
+	
+	def __get_type(self):
+		return self.__type
+	
+	def __set_is_declaration(self, is_declaration):
+		self.__is_declaration = is_declaration
+	
+	def __set_sub_type(self, sub_type):
+		self.__sub_type = sub_type
+	
+	def __set_type(self, type):
+		self.__type = type
+	
+	identifier = property(__get_identifier)
+	is_declaration = property(__get_is_declaration, __set_is_declaration)
+	parts = property(__get_parts)
+	sub_type = property(__get_sub_type, __set_sub_type)
+	type = property(__get_type, __set_type)
+
+class Part(object):
+	def __init__(self, name, type_identifier):
+		self.__type_identifier = type_identifier
+		self.__name = name
+	
+	def __get_type_identifier(self):
+		return self.__type_identifier
+	
+	def __get_name(self):
+		return self.__name
+	
+	type_identifier = property(__get_type_identifier)
+	name = property(__get_name)
+
 # read the declarations first
 declarations = dict();
 if options.declarations != None:
 	declarations_document = parse(options.declarations)
 	declarations_element = declarations_document.documentElement
 	for declaration_element in declarations_element.childNodes:
-		if declaration_element.nodeType == Node.ELEMENT_NODE and declarations.has_key(declaration_element.tagName) == False:
-			if declaration_element.attributes.has_key("is") == True:
-				declarations[declaration_element.tagName] = declaration_element.attributes.get("is").nodeValue
+		if declaration_element.nodeType == Node.ELEMENT_NODE:
+			if declarations.has_key(declaration_element.tagName) == False:
+				declaration = Declaration(declaration_element.tagName)
+				if declaration_element.attributes.has_key("type") == True:
+					declaration.type = int(declaration_element.attributes.get("type").nodeValue)
+				if declaration_element.attributes.has_key("sub-type") == True:
+					declaration.sub_type = int(declaration_element.attributes.get("sub_type").nodeValue)
+				if declaration_element.attributes.has_key("is") == True:
+					declaration.is_declaration = declaration_element.attributes.get("is").nodeValue
+				else:
+					for part_element in declaration_element.childNodes:
+						if part_element.nodeType == Node.ELEMENT_NODE:
+							if part_element.attributes.has_key("identifier") == True:
+								declaration.parts.append(Part(part_element.attributes.get("identifier").nodeValue, part_element.tagName))
+							else:
+								print LightRed + "Error" + White + ": In file " + LightYellow + options.declarations + White + " there is a declaration part without an " + LightYellow + "identifier" + White + " attribute on the " + LightYellow + declaration_element.tagName + White + " declaration."
+								raise ConvertException()
+				declarations[declaration.identifier] = declaration
 			else:
-				declaration = list()
-				for part_element in declaration_element.childNodes:
-					if part_element.nodeType == Node.ELEMENT_NODE and part_element.attributes.has_key("identifier") == True:
-						declaration.append((part_element.attributes.get("identifier").nodeValue, part_element.tagName))
-				declarations[declaration_element.tagName] = declaration
+				print LightRed + "Error" + White + ": In file " + LightYellow + options.declarations + White + " there is a duplicate declaration for " + LightYellow + declaration_element.tagName + White + "."
+				raise ConvertException()
 
 # now open the out file for writing binary
 out_file = open(options.out_file, "wb")
@@ -106,29 +170,29 @@ def out(data_type, node):
 			print LightRed + "Error" + White + ": In file " + LightYellow + options.in_file + White + " for " + LightYellow + stack_path + White + " of type " + DarkYellow + data_type + White + " I couldn't open the file: " + LightRed + inline_file_name + White
 			raise ConvertException()
 	elif declarations.has_key(data_type) == True:
-		if isinstance(declarations[data_type], ListType):
+		if declarations[data_type].is_declaration != None:
+			out(declarations[data_type].is_declaration, node)
+		else:
 			for child_node in node.childNodes:
 				if child_node.nodeType != Node.ELEMENT_NODE:
 					node.removeChild(child_node)
-			parts = map(None, declarations[data_type], node.childNodes)
-			for declaration, definition_node in parts:
+			# this fills the parts list with pairs of values from the first and second list extending with None if necessary
+			parts = map(None, declarations[data_type].parts, node.childNodes)
+			for declaration_part, definition_node in parts:
 				# safe-guard
-				assert declaration != None or definition_node != None
-				if declaration == None:
+				assert declaration_part != None or definition_node != None
+				if declaration_part == None:
 					print LightRed + "Error" + White + ": In file " + LightYellow + options.in_file + White + " for " + LightYellow + stack_path + "/" + definition_node.tagName + White + " I could not find any declaration."
 					raise ConvertException()
-				declaration_name, declaration_type = declaration
 				if definition_node == None:
-					print LightRed + "Error" + White + ": In file " + LightYellow + options.in_file + White + " for " + LightYellow + stack_path + "/" + declaration_name + White + " of type " + DarkYellow + declaration_type + White + " I could not find any definition."
+					print LightRed + "Error" + White + ": In file " + LightYellow + options.in_file + White + " for " + LightYellow + stack_path + "/" + declaration_part.name + White + " of type " + DarkYellow + declaration_part.type_identifier + White + " I could not find any definition."
 					raise ConvertException()
-				if declaration_name != definition_node.tagName:
+				if declaration_part.name != definition_node.tagName:
 					print LightRed + "Error" + White + ": In file " + LightYellow + options.in_file + White + " the definition for " + LightYellow + stack_path + "/" + definition_node.tagName + White + " does not belong there."
-					print "               Expected to find a definition for " + LightYellow + stack_path + "/" + declaration_name + White + " of type " + DarkYellow + declaration_type + White + "'."
+					print "               Expected to find a definition for " + LightYellow + stack_path + "/" + declaration_part.name + White + " of type " + DarkYellow + declaration_part.type_identifier + White + "'."
 					raise ConvertException()
 				else:
-					out(declaration_type, definition_node)
-		elif isinstance(declarations[data_type], StringTypes):
-			out(declarations[data_type], node)
+					out(declaration_part.type_identifier, definition_node)
 	else:
 		print LightRed + "Error" + White + ": In file " + LightYellow + options.in_file + White + " for " + LightYellow + stack_path + White + " of type " + DarkYellow + data_type + White + " I could not find a suitable declaration."
 		raise ConvertException()
