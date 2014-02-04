654d446026e2f2871c81c81c7058a089f72eb8d5
Hagen Möbius <hagen.moebius@googlemail.com>
2014-02-04 21:47:20 +0100

    The "Mount" and "Unmount" buttons are now updated via the widget updating mechanism.

--- source/ui/outfit_ship_dialog.h
+++ source/ui/outfit_ship_dialog.h
@@ -40,13 +40,14 @@ namespace UI
 		bool _OnAccessoryListItemMouseButton(UI::AccessoryListItem * AccessoryListItem, int Button, int State, float X, float Y);
 		bool _OnKey(const KeyEventInformation & KeyEventInformation);
 		void _OnMountButtonClicked(void);
+		void _OnMountButtonUpdating(UI::Button * MountButton, float RealTimeSeconds, float GameTimeSeconds);
 		void _OnOKButtonClicked(void);
 		void _OnSizeChanged(void);
 		bool _OnSlotListItemMouseButton(UI::SlotListItem * SlotListItem, int Button, int State, float X, float Y);
 		void _OnUnmountButtonClicked(void);
+		void _OnUnmountButtonUpdating(UI::Button * UnmountButton, float RealTimeSeconds, float GameTimeSeconds);
 		// helper function and actions
 		void _RebuildAccessoryList(void);
-		void _UpdateButtons(void);
 		// member variables
 		UI::ScrollBox * _AccessoryScrollBox;
 		UI::Widget * _CenterPane;
