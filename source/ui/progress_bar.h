19669d5a40c7e67aabc3ebeb5e4dfefa9f89c9e8
Hagen M?bius <hagen.moebius@googlemail.com>
2014-02-20 01:11:20 +0100

    Added a new widget class ProgressBar. It features three settings (text, color and fill level). Used the new ProgressBar class to simplify the HangarWidget.

---
+++ source/ui/progress_bar.h
@@ -0,0 +1,47 @@
+/**
+ * galactic-fall
+ * Copyright (C) 2014  Hagen Möbius
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef UI_PROGRESS_BAR_H
+#define UI_PROGRESS_BAR_H
+
+#include "widget.h"
+
+namespace UI
+{
+	class Label;
+	
+	class ProgressBar : public UI::Widget
+	{
+	public:
+		ProgressBar(UI::Widget * SupWidget);
+		// setters
+		void SetColor(const Color & Color);
+		void SetFillLevel(float FillLevel);
+		void SetText(const std::string & Text);
+	private:
+		void _OnSizeChanged(void);
+		void _ResizeFill(void);
+		// variables
+		UI::Widget * _Fill;
+		float _FillLevel;
+		UI::Label * _Label;
+	};
+}
+
+#endif
