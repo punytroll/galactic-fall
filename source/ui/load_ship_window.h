f4434f339e0565cccbbff6b1e167bd913b0a3a70
Hagen Möbius <hagen.moebius@googlemail.com>
2014-07-13 11:23:06 +0200

    Added a new button to the hangar widget that opens up a LoadShipWindow. The LoadShipWindow allows transfering objects between the hangar and the ship cargo hold. This window uses a few new concepts to keep the content lists for both hangar and ship updated, even if changes to the content are initiated in other windows. Basically, the lists can be considered as views on the content model of hangar or ship cargo hold, reacting to added and removed events by reorganizing the list items. This works all very well, but currently the game fails, if the hangar is not empty when taking off.

---
+++ source/ui/load_ship_window.h
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2014  Hagen Möbius
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef LOAD_SHIP_WINDOW_H
+#define LOAD_SHIP_WINDOW_H
+
+#include "../connection.h"
+#include "window.h"
+
+class Hangar;
+class Ship;
+
+namespace UI
+{
+	class Button;
+	class ObjectListItem;
+	class ScrollBox;
+	
+	class LoadShipWindow : public UI::Window
+	{
+	public:
+		// constructor and destructor
+		LoadShipWindow(UI::Widget * SupWidget, Hangar * Hangar, Ship * Ship);
+	private:
+		// event handler
+		void _OnDestroying(UI::Event & DestroyingEvent);
+		void _OnHangarContentAdded(Object * Content);
+		void _OnHangarContentRemoved(Object * Content);
+		void _OnHangarDestroying(void);
+		void _OnHangarItemMouseButton(UI::MouseButtonEvent & MouseButtonEvent, UI::ObjectListItem * HangarItem);
+		void _OnHangarScrollBoxSubWidgetAdded(UI::SubWidgetEvent & SubWidgetEvent);
+		void _OnHangarScrollBoxSubWidgetRemoved(UI::SubWidgetEvent & SubWidgetEvent);
+		void _OnKey(UI::KeyEvent & KeyEvent);
+		void _OnMoveToHangarButtonClicked(void);
+		void _OnMoveToHangarButtonUpdating(float RealTimeSeconds, float GameTimeSeconds, UI::Button * MoveToHangarButton);
+		void _OnMoveToShipButtonClicked(void);
+		void _OnMoveToShipButtonUpdating(float RealTimeSeconds, float GameTimeSeconds, UI::Button * MoveToShipButton);
+		void _OnOKButtonClicked(void);
+		void _OnShipContentAdded(Object * Content);
+		void _OnShipContentRemoved(Object * Content);
+		void _OnShipDestroying(void);
+		void _OnShipItemMouseButton(UI::MouseButtonEvent & MouseButtonEvent, UI::ObjectListItem * ShipItem);
+		void _OnShipScrollBoxSubWidgetAdded(UI::SubWidgetEvent & SubWidgetEvent);
+		void _OnShipScrollBoxSubWidgetRemoved(UI::SubWidgetEvent & SubWidgetEvent);
+		void _OnSizeChanged(UI::Event & SizeChangedEvent, UI::Widget * LeftPane, UI::Widget * CenterPane, UI::Widget * RightPane);
+		// member variables
+		Hangar * _Hangar;
+		Connection _HangarContentAddedCallbackConnection;
+		Connection _HangarContentRemovedCallbackConnection;
+		Connection _HangarDestroyingCallbackConnection;
+		UI::ScrollBox * _HangarScrollBox;
+		UI::ObjectListItem * _SelectedHangarItem;
+		UI::ObjectListItem * _SelectedShipItem;
+		Ship * _Ship;
+		Connection _ShipContentAddedCallbackConnection;
+		Connection _ShipContentRemovedCallbackConnection;
+		Connection _ShipDestroyingCallbackConnection;
+		UI::ScrollBox * _ShipScrollBox;
+	};
+}
+
+#endif
