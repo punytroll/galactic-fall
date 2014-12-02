56744032ca6b67eb8a65cfe15010633c551ed758
Hagen M?bius <hagen.moebius@googlemail.com>
2014-12-02 23:56:33 +0100

    Removed the non-essential "--info" parameter.

--- tools/littlem/source/littlem.cpp
+++ tools/littlem/source/littlem.cpp
@@ -3219,16 +3219,6 @@ int main(int argc, char ** argv)
 	glutPassiveMotionFunc(vPassiveMouseMotion);
 	glutReshapeFunc(vReshape);
 	vSetupUserInterface();
-	for(auto Argument : std::vector< std::string >(argv + 1, argv + argc))
-	{
-		if(Argument == "--info")
-		{
-			std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
-			std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-			std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
-			std::cout << "Extensions: " << glGetString(GL_EXTENSIONS) << std::endl;
-		}
-	}
 	glutMainLoop();
 	
 	return 0;
