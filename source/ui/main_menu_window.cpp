4b81200a4955245036cc91420974c56e25c033c4
Hagen M?bius <hagen.moebius@googlemail.com>
2018-02-02 18:36:36 +0100

    The TextButton and MainMenuWindow classes are now completely free of anchors.

--- source/ui/main_menu_window.cpp
+++ source/ui/main_menu_window.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013  Hagen Möbius
+ * Copyright (C) 2013-2018  Hagen Möbius
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -67,77 +67,53 @@ UI::MainMenuWindow::MainMenuWindow(UI::Widget * SupWidget, ScenarioManager * Sce
 	_ResumeGameButton->SetName("resume_game");
 	_ResumeGameButton->SetLeft(20.0_c);
 	_ResumeGameButton->SetTop(60.0_c);
-	_ResumeGameButton->SetWidth(constant(GetWidth() - 40.0f));
+	_ResumeGameButton->SetWidth(width(this) - 40.0_c);
 	_ResumeGameButton->SetHeight(20.0_c);
-	_ResumeGameButton->SetAnchorBottom(false);
-	_ResumeGameButton->SetAnchorLeft(true);
-	_ResumeGameButton->SetAnchorRight(true);
-	_ResumeGameButton->SetAnchorTop(true);
 	_ResumeGameButton->ConnectClickedCallback(std::bind(&UI::MainMenuWindow::_OnResumeGameButtonClicked, this));
 	
 	// "New Game" button
 	_NewGameButton = new UI::TextButton{this, "New Game"};
 	_NewGameButton->SetName("new_game");
 	_NewGameButton->SetLeft(20.0_c);
-	_NewGameButton->SetTop(constant(_ResumeGameButton->GetBottom() + 20.0f));
-	_NewGameButton->SetWidth(constant(GetWidth() - 40.0f));
+	_NewGameButton->SetTop(bottom(_ResumeGameButton) + 20.0_c);
+	_NewGameButton->SetWidth(width(this) - 40.0_c);
 	_NewGameButton->SetHeight(20.0_c);
-	_NewGameButton->SetAnchorBottom(false);
-	_NewGameButton->SetAnchorLeft(true);
-	_NewGameButton->SetAnchorRight(true);
-	_NewGameButton->SetAnchorTop(true);
 	_NewGameButton->ConnectClickedCallback(std::bind(&UI::MainMenuWindow::_OnNewGameButtonClicked, this));
 	
 	// "Load Scenario" button
 	_LoadScenarioButton = new UI::TextButton{this, "Load Scenario"};
 	_LoadScenarioButton->SetName("load_scenario");
 	_LoadScenarioButton->SetLeft(20.0_c);
-	_LoadScenarioButton->SetTop(constant(_NewGameButton->GetBottom() + 20.0f));
-	_LoadScenarioButton->SetWidth(constant(GetWidth() - 40.0f));
+	_LoadScenarioButton->SetTop(bottom(_NewGameButton) + 20.0_c);
+	_LoadScenarioButton->SetWidth(width(this) - 40.0_c);
 	_LoadScenarioButton->SetHeight(20.0_c);
-	_LoadScenarioButton->SetAnchorBottom(false);
-	_LoadScenarioButton->SetAnchorLeft(true);
-	_LoadScenarioButton->SetAnchorRight(true);
-	_LoadScenarioButton->SetAnchorTop(true);
 	_LoadScenarioButton->ConnectClickedCallback(std::bind(&UI::MainMenuWindow::_OnLoadScenarioButtonClicked, this));
 	
 	// "Load Game" button
 	_LoadGameButton = new UI::TextButton{this, "Load Game"};
 	_LoadGameButton->SetName("load_game");
 	_LoadGameButton->SetLeft(20.0_c);
-	_LoadGameButton->SetTop(constant(_LoadScenarioButton->GetBottom() + 20.0f));
-	_LoadGameButton->SetWidth(constant(GetWidth() - 40.0f));
+	_LoadGameButton->SetTop(bottom(_LoadScenarioButton) + 20.0_c);
+	_LoadGameButton->SetWidth(width(this) - 40.0_c);
 	_LoadGameButton->SetHeight(20.0_c);
-	_LoadGameButton->SetAnchorBottom(false);
-	_LoadGameButton->SetAnchorLeft(true);
-	_LoadGameButton->SetAnchorRight(true);
-	_LoadGameButton->SetAnchorTop(true);
 	_LoadGameButton->ConnectClickedCallback(std::bind(&UI::MainMenuWindow::_OnLoadGameButtonClicked, this));
 	
 	// "Save Game" button
 	_SaveGameButton = new UI::TextButton{this, "Save Game"};
 	_SaveGameButton->SetName("save_game");
 	_SaveGameButton->SetLeft(20.0_c);
-	_SaveGameButton->SetTop(constant(_LoadGameButton->GetBottom() + 20.0f));
-	_SaveGameButton->SetWidth(constant(GetWidth() - 40.0f));
+	_SaveGameButton->SetTop(bottom(_LoadGameButton) + 20.0_c);
+	_SaveGameButton->SetWidth(width(this) - 40.0_c);
 	_SaveGameButton->SetHeight(20.0_c);
-	_SaveGameButton->SetAnchorBottom(false);
-	_SaveGameButton->SetAnchorLeft(true);
-	_SaveGameButton->SetAnchorRight(true);
-	_SaveGameButton->SetAnchorTop(true);
 	_SaveGameButton->ConnectClickedCallback(std::bind(&UI::MainMenuWindow::_OnSaveGameButtonClicked, this));
 	
 	// "Quit" button
 	_QuitButton = new UI::TextButton{this, "Quit"};
 	_QuitButton->SetName("quit");
 	_QuitButton->SetLeft(20.0_c);
-	_QuitButton->SetTop(constant(_SaveGameButton->GetBottom() + 20.0f));
-	_QuitButton->SetWidth(constant(GetWidth() - 40.0f));
+	_QuitButton->SetTop(bottom(_SaveGameButton) + 20.0_c);
+	_QuitButton->SetWidth(width(this) - 40.0_c);
 	_QuitButton->SetHeight(20.0_c);
-	_QuitButton->SetAnchorBottom(false);
-	_QuitButton->SetAnchorLeft(true);
-	_QuitButton->SetAnchorRight(true);
-	_QuitButton->SetAnchorTop(true);
 	_QuitButton->ConnectClickedCallback(std::bind(&UI::MainMenuWindow::_OnQuitButtonClicked, this));
 }
 
