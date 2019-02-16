906b966824c3c7152aa418a7f7f967bc9daaf259
Hagen Möbius <hagen.moebius@googlemail.com>
2019-02-16 14:15:52 +0100

    Made the position of markers optional. Reading and writing of markers now work with optional elements. Also display a selected marker's orientation, if it has one.

--- tools/littlem/source/marker.h
+++ tools/littlem/source/marker.h
@@ -1,8 +1,10 @@
 #ifndef LITTLEM_MARKER_H
 #define LITTLEM_MARKER_H
 
+#include <experimental/optional>
 #include <string>
 
+#include <algebra/quaternion.h>
 #include <algebra/vector3f.h>
 
 class Marker
@@ -13,14 +15,38 @@ public:
 		return _Identifier;
 	}
 	
+	const Quaternion & GetOrientation(void) const
+	{
+		assert(_Orientation);
+		
+		return _Orientation.value();
+	}
+	
 	const Vector3f & GetPosition(void) const
 	{
-		return _Position;
+		assert(_Position);
+		
+		return _Position.value();
+	}
+	
+	bool HasOrientation(void) const
+	{
+		return (bool)_Orientation;
+	}
+	
+	bool HasPosition(void) const
+	{
+		return (bool)_Position;
 	}
 	
 	void SetIdentifier(const std::string & Identifier)
 	{
 		_Identifier = Identifier;
+	}
+	
+	void SetOrientation(const Quaternion & Orientation)
+	{
+		_Orientation = Orientation;
 	}
 	
 	void SetPosition(const Vector3f & Position)
@@ -29,7 +55,8 @@ public:
 	}
 private:
 	std::string _Identifier;
-	Vector3f _Position;
+	std::experimental::optional< Quaternion > _Orientation;
+	std::experimental::optional< Vector3f > _Position;
 };
 
 #endif
