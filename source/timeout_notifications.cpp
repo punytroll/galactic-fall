ec1c44678e2473016ae77c15fe218ade6aa2ec7b
Hagen Möbius <hagen.moebius@googlemail.com>
2019-02-22 11:12:47 +0100

    Cleaned up the timeout notifications.

--- source/timeout_notifications.cpp
+++ source/timeout_notifications.cpp
@@ -1,6 +1,6 @@
 /**
  * galactic-fall
- * Copyright (C) 2007  Hagen Möbius
+ * Copyright (C) 2007-2019  Hagen Möbius
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -57,19 +57,19 @@ public:
 	
 	void Dismiss(void)
 	{
-		assert(_Manager != 0);
+		assert(_Manager != nullptr);
 		_Manager->Remove(_Iterator);
 		Invalidate();
 	}
 	
 	void Invalidate(void)
 	{
-		_Manager = 0;
+		_Manager = nullptr;
 	}
 	
 	bool IsValid(void) const
 	{
-		return _Manager != 0;
+		return _Manager != nullptr;
 	}
 	
 	void Call(void)
@@ -84,24 +84,24 @@ private:
 };
 
 TimeoutNotification::TimeoutNotification(void) :
-	_Core(0)
+	_Core(nullptr)
 {
 }
 
 TimeoutNotification::TimeoutNotification(const TimeoutNotification & TimeoutNotification) :
-	_Core(0)
+	_Core(nullptr)
 {
 	_Core = TimeoutNotification._Core;
-	if(_Core != 0)
+	if(_Core != nullptr)
 	{
 		_Core->Reference();
 	}
 }
 
 TimeoutNotification::TimeoutNotification(TimeoutNotificationManager * Manager, std::multimap< double, TimeoutNotification * >::iterator Iterator, std::function< void (void) > Callback) :
-	_Core(0)
+	_Core(nullptr)
 {
-	_Core = new TimeoutNotificationCore(Manager, Iterator, Callback);
+	_Core = new TimeoutNotificationCore{Manager, Iterator, Callback};
 	_Core->Reference();
 }
 
@@ -121,18 +121,18 @@ TimeoutNotification::~TimeoutNotification(void)
 TimeoutNotification & TimeoutNotification::operator=(const TimeoutNotification & TimeoutNotification)
 {
 	// dereference old core
-	if(_Core != 0)
+	if(_Core != nullptr)
 	{
 		_Core->Unreference();
 		if(_Core->GetReferences() == 0)
 		{
 			delete _Core;
 		}
-		_Core = 0;
+		_Core = nullptr;
 	}
 	// set and reference new core
 	_Core = TimeoutNotification._Core;
-	if(_Core != 0)
+	if(_Core != nullptr)
 	{
 		_Core->Reference();
 	}
@@ -142,31 +142,31 @@ TimeoutNotification & TimeoutNotification::operator=(const TimeoutNotification &
 
 bool TimeoutNotification::IsValid(void) const
 {
-	return (_Core != 0) && (_Core->IsValid() == true);
+	return (_Core != nullptr) && (_Core->IsValid() == true);
 }
 
 void TimeoutNotification::Dismiss(void)
 {
-	assert(_Core != 0);
+	assert(_Core != nullptr);
 	_Core->Dismiss();
 }
 
 void TimeoutNotification::Call(void)
 {
-	assert(_Core != 0);
+	assert(_Core != nullptr);
 	_Core->Call();
 }
 
 void TimeoutNotification::Invalidate(void)
 {
-	assert(_Core != 0);
+	assert(_Core != nullptr);
 	_Core->Invalidate();
 	_Core->Unreference();
 	if(_Core->GetReferences() == 0)
 	{
 		delete _Core;
 	}
-	_Core = 0;
+	_Core = nullptr;
 }
 
 TimeoutNotificationManager::~TimeoutNotificationManager(void)
@@ -182,10 +182,10 @@ void TimeoutNotificationManager::Process(double Time)
 {
 	while((_TimeoutNotifications.size() > 0) && (Time > _TimeoutNotifications.begin()->first))
 	{
-		std::multimap< double, TimeoutNotification * >::iterator Iterator(_TimeoutNotifications.begin());
+		auto Iterator{_TimeoutNotifications.begin()};
 		
 		// only valid timeout notifications in here
-		assert(Iterator->second != 0);
+		assert(Iterator->second != nullptr);
 		assert(Iterator->second->IsValid() == true);
 		// call the notification callback object
 		Iterator->second->Call();
@@ -196,8 +196,8 @@ void TimeoutNotificationManager::Process(double Time)
 
 TimeoutNotification TimeoutNotificationManager::Add(double Time, std::function< void (void) > Callback)
 {
-	std::multimap< double, TimeoutNotification * >::iterator Iterator(_TimeoutNotifications.insert(std::make_pair(Time, reinterpret_cast< TimeoutNotification * >(0))));
-	TimeoutNotification * Result(new TimeoutNotification(this, Iterator, Callback));
+	auto Iterator{_TimeoutNotifications.insert(std::make_pair(Time, static_cast< TimeoutNotification * >(nullptr)))};
+	auto Result{new TimeoutNotification{this, Iterator, Callback}};
 	
 	Iterator->second = Result;
 	
