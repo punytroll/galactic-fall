a938ebe0c0b4f5b8e0161f37ff90f0627c56b367
Hagen Möbius <hagen.moebius@googlemail.com>
2019-08-09 23:00:16 +0200

    The TrianglePoint class is now hiding all member fields.

--- tools/littlem/source/triangle_point.cpp
+++ tools/littlem/source/triangle_point.cpp
@@ -1,12 +1,11 @@
 #include <algorithm>
 #include <cassert>
 
-#include <GL/gl.h>
-
 #include "point.h"
 #include "triangle_point.h"
 
 TrianglePoint::TrianglePoint(void) :
+	_Normal(Vector3f::CreateFromComponents(1.0f, 0.0f, 0.0f)),
 	_Point(nullptr)
 {
 }
@@ -17,21 +16,6 @@ TrianglePoint::~TrianglePoint(void)
 	{
 		_Point->_TrianglePoints.erase(std::find(_Point->_TrianglePoints.begin(), _Point->_TrianglePoints.end(), this));
 	}
-}
-
-Point * TrianglePoint::GetPoint(void)
-{
-	return _Point;
-}
-
-const Point * TrianglePoint::GetPoint(void) const
-{
-	return _Point;
-}
-
-const std::vector< Triangle * > & TrianglePoint::GetTriangles(void) const
-{
-	return _Triangles;
 }
 
 void TrianglePoint::SetPoint(Point * Point)
